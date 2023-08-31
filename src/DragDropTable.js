dc237f9484c3f135745cf6ec10e30a74102800be
Seregina508 <esgn508@gmail.com>
2023-08-30 22:25:42 -0400

    added to DDTbl

--- src/DragDropTable.js
+++ src/DragDropTable.js
@@ -1,16 +1,160 @@
+// import React, {useState} from 'react';
+// import {
+//     Button,
+//     Checkbox,
+//     FormControlLabel,
+//     Table,
+//     TableBody,
+//     TableContainer,
+//     TableHead,
+//     TableRow,
+//     TextField
+// } from "@mui/material";
+// import DraggableRow from "./DraggableRow";
+//
+// const DragDropTable = () => {
+//
+//     const initialColumns = ['Row Name', 'Data'];
+//     const initialData = [
+//         {'Row Name': 'Row 1', Data: 'Data 1', editable: false},
+//         {'Row Name': 'Row 2', Data: 'Data 2', editable: false},
+//         {'Row Name': 'Row 3', Data: 'Data 3', editable: false},
+//     ]
+//
+//     const [columns, setColumns] = useState(initialColumns);
+//     const [tableData, setTableData] = useState(initialData);
+//     const [newRow, setNewRow] = useState({});
+//     const [showAddRowForms, setShowAddRowForms] = useState(false);   //
+//     const [newColumn, setNewColumn] = useState('');
+//     const [showColumnInputs, setShowColumnInputs] = useState(false);
+//
+//     //чекбокс
+//
+//     const handleCheckboxChange = (event) => {
+//         const columnName = event.target.name;
+//         if (event.target.checked) {
+//             setColumns([...columns, columnName])
+//         } else {
+//             setColumns(columns.filter((col) => col !== columnName))
+//         }
+//     }
+//
+//     const handleNewColumnChange = (event) => {
+//         setNewColumn(event.target.value);
+//     }
+//
+//     const handleAddColumn = () => {
+//         if (newColumn.trim() !== '') {
+//             setColumns([...columns, newColumn])
+//             setNewColumn('')
+//         }
+//     }
+//
+//     //строки
+//
+//     const handleNewRowChange = (event) => {
+//         setNewRow({...newRow, [event.target.name]: event.target.value});
+//     }
+//
+//     const handleAddRow = () => {
+//         setTableData([...tableData, {...newRow, editable: false}]);
+//         setNewRow({});
+//         setShowAddRowForms(false)
+//     }
+//
+//     const handleEditRow = (index, col, value) => {
+//         const newData = [...tableData];
+//         newData[index][col] = value;
+//         setTableData(newData);
+//     }
+//
+//     //перетаскивание строк (замена строк местами)
+//
+//     const handleMoveRow = (fromIndex, toIndex) => {
+//         const newData = [...tableData];
+//         newData.splice(toIndex, 0, newData.splice(fromIndex, 1)[0])
+//     }
+//
+//     return (
+//         <>
+//             <div>
+//                 {/*add columns*/}
+//                 <Button>Add Column</Button>
+//
+//                 {/*циклом map колонки по индексу*/}
+//
+//                 <TextField
+//                     label="New Column"
+//                 />
+//
+//                 <Button>Add</Button>
+//
+//                 <div>
+//                     <FormControlLabel
+//
+//                         control={
+//                             <Checkbox
+//
+//                             />
+//                         }
+//
+//                         // label column
+//
+//                     />
+//                 </div>
+//
+//
+//                 <div>
+//                     {/*add Row*/}
+//                     <Button>Add Row</Button>
+//
+//                     <TextField
+//                         label="New Row"
+//                     />
+//
+//                     <Button>Add Row</Button>
+//
+//                 </div>
+//             </div>
+//
+//             <TableContainer>
+//                 <Table>
+//                     <TableHead>
+//                         <TableRow>
+//                             {/*Cells*/}
+//                         </TableRow>
+//                     </TableHead>
+//
+//                     <TableBody>
+//
+//                         <DraggableRow/>
+//
+//                     </TableBody>
+//                 </Table>
+//             </TableContainer>
+//
+//         </>
+//     );
+// };
+//
+// export default DragDropTable;
+
+
 import React, {useState} from 'react';
 import {
     Button,
     Checkbox,
-    FormControlLabel,
+    FormControlLabel, Paper,
     Table,
-    TableBody,
+    TableBody, TableCell,
     TableContainer,
     TableHead,
     TableRow,
     TextField
 } from "@mui/material";
 import DraggableRow from "./DraggableRow";
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
 const DragDropTable = () => {
 
@@ -19,122 +163,173 @@ const DragDropTable = () => {
         {'Row Name': 'Row 1', Data: 'Data 1', editable: false},
         {'Row Name': 'Row 2', Data: 'Data 2', editable: false},
         {'Row Name': 'Row 3', Data: 'Data 3', editable: false},
-    ]
+    ];
 
     const [columns, setColumns] = useState(initialColumns);
     const [tableData, setTableData] = useState(initialData);
     const [newRow, setNewRow] = useState({});
-    const [showAddRowForms, setShowAddRowForms] = useState(false);   //
-    const [newColumn, setNewColumn] = useState('');
-    const [showColumnInputs, setShowColumnInputs] = useState(false);
+    const [showAddRowForm, setShowAddRowForm] = useState(false);
+    const [newColumn, setNewCollumn] = useState('');
+    const [showColumnInput, setShowColumnInput] = useState(false);
 
-    //чекбокс
+    // checkbox
 
-    const handleCheckboxChange = (event) => {
+    const handleCheckBoxChange = (event) => {
         const columnName = event.target.name;
         if (event.target.checked) {
             setColumns([...columns, columnName])
         } else {
-            setColumns(columns.filter((col) => col !== columnName))
+            setColumns(columns.filter(col => col !== columnName))
         }
     }
 
     const handleNewColumnChange = (event) => {
-        setNewColumn(event.target.value);
+        setNewCollumn(event.target.value)
     }
 
     const handleAddColumn = () => {
         if (newColumn.trim() !== '') {
             setColumns([...columns, newColumn])
-            setNewColumn('')
+            setNewCollumn('');
         }
     }
 
     //строки
 
     const handleNewRowChange = (event) => {
-        setNewRow({...newRow, [event.target.name]: event.target.value});
+        setNewRow({...newRow, [event.target.name]: event.target.value})
     }
 
     const handleAddRow = () => {
-        setTableData([...tableData, {...newRow, editable: false}]);
+        setTableData([...tableData, {...newRow, editable: false}])
         setNewRow({});
-        setShowAddRowForms(false)
+        setShowAddRowForm(false)
     }
 
     const handleEditRow = (index, col, value) => {
         const newData = [...tableData];
         newData[index][col] = value;
-        setTableData(newData);
+        setTableData(newData)
     }
 
-    //перетаскивание строк (замена строк местами)
+    //замена строк местами
 
     const handleMoveRow = (fromIndex, toIndex) => {
         const newData = [...tableData];
         newData.splice(toIndex, 0, newData.splice(fromIndex, 1)[0])
     }
 
+
     return (
-        <>
+        <DndProvider backend={HTML5Backend}>
             <div>
-                {/*add columns*/}
-                <Button>Add Column</Button>
+                <Button onClick={() => setShowColumnInput(!showColumnInput)}>Add Column</Button>
+                {/*map по колонкам*/}
 
-                {/*циклом map колонки по индексу*/}
+                {showColumnInput && (
+                    <div>
+                        <TextField
+                            label='New Column'
+                            value={newColumn}
+                            onChange={handleNewColumnChange}
+                        />
 
-                <TextField
-                    label="New Column"
-                />
+                        <Button onClick={handleAddColumn}>Add</Button>
 
-                <Button>Add</Button>
+                        <div>
 
-                <div>
-                    <FormControlLabel
+                            {columns.map((col, index) => (
+                                <FormControlLabel
+                                    key={index}
+                                    control={
+                                        <Checkbox
+                                            checked={columns.includes(col)}
+                                            onChange={handleCheckBoxChange}
+                                            name={col}
+                                        />
+                                    }
+                                    // label колонки
+                                    label={col}
+                                />
+                            ))}
+                        </div>
+                    </div>
+                )}
 
-                        control={
-                            <Checkbox
 
+                <Button onClick={() => setShowAddRowForm(!showAddRowForm)}>Add Row</Button>
+
+                {showAddRowForm && (
+                    <div>
+                        {columns.map((col, index) => (
+                            <TextField
+                                key={index}
+                                label={col}
+                                name={col}
+                                value={newRow[col] || ''}
+                                onChange={handleNewRowChange}
                             />
-                        }
-
-                        // label column
-
-                    />
-                </div>
-
-
-                <div>
-                    {/*add Row*/}
-                    <Button>Add Row</Button>
-
-                    <TextField
-                        label="New Row"
-                    />
-
-                    <Button>Add Row</Button>
-
-                </div>
+                        ))}
+                        <Button onClick={handleAddRow}>Add Row</Button>
+                    </div>
+                )}
             </div>
 
-            <TableContainer>
+            <TableContainer component={Paper}>
                 <Table>
                     <TableHead>
                         <TableRow>
-                            {/*Cells*/}
+                            {/*ячейки*/}
+                            {columns.map((col, ind) => (
+                                <TableCell key={ind}>{col}</TableCell>
+                            ))}
                         </TableRow>
                     </TableHead>
 
                     <TableBody>
-
-                        <DraggableRow/>
-
+                        {tableData.map((row, index) => (
+                            <DraggableRow
+                                key={index}
+                                row={row}
+                                index={index}
+                                columns={columns}
+                                handleEditRow={handleEditRow}
+                                handleMoveRow={handleMoveRow}
+                            />
+                        ))}
                     </TableBody>
                 </Table>
             </TableContainer>
 
-        </>
+        </DndProvider>
     );
 };
 
 export default DragDropTable;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/DraggableRow.js
+++ src/DraggableRow.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import {useDrag, useDrop} from "react-dnd";
+import {TableCell, TableRow, TextField} from "@mui/material";
 
-const DraggableRow = () => {
+const ItemType = 'Table_Row';
+
+const DraggableRow = ({row, index, handleEditRow, handleMoveRow, columns}) => {
+    const [, ref] = useDrag({
+        type: ItemType,
+        item: {index}
+    })
+
+    const [, drop] = useDrop({
+        accept: ItemType,
+        hover: (draggedItem) => {
+            if (draggedItem.index !== index) {
+                handleMoveRow(draggedItem.index, index)
+                draggedItem.index = index
+            }
+        }
+    })
+
     return (
-        <>
-            
-        </>
+        <TableRow>
+            {columns.map((col, ind) => (
+                <TableCell key={ind}>
+                    {/*<TextField/>*/}
+                </TableCell>
+            ))}
+        </TableRow>
     );
 };
 
